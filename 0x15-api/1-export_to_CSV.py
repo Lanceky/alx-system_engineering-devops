a2ff23f48c4ab7bc731a219976e9328c64fa61b9
Lance <evanslance100@gmail.com>
2024-07-29 22:40:58 -0700

    removed whitespce

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -3,8 +3,8 @@
 """
 Exports TODO list data for a specific employee to a CSV file.
 
-This script takes an employee ID as a command-line argument and writes the 
-employee's TODO list data to a CSV file named <employee_id>.csv. The CSV file 
+This script takes an employee ID as a command-line argument and writes the
+employee's TODO list data to a CSV file named <employee_id>.csv. The CSV file
 includes columns for task ID, title, and completion status.
 """
 
@@ -24,7 +24,9 @@ def export_to_csv(employee_id):
         return
 
     user_name = user.get('username')
-    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    todos_url = (
+        f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    )
     todos_response = requests.get(todos_url)
     todos = todos_response.json()
 
