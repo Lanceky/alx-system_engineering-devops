0c6770ec4d8c086c1fc742d0cfc744f7c1dfefc8
Lance <evanslance100@gmail.com>
2024-08-22 20:28:21 +0000

    remove typo

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,14 +1,15 @@
-ky_is_the_limit_not.pp
-# Increases the ULIMIT of the default file for Nginx
+# 0-the_sky_is_the_limit_not.pp
+# Increases the ULIMIT for the Nginx process
 
-# Increase the ULIMIT of the default file
+# Increase the ULIMIT in the default Nginx configuration
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
+  path    => '/usr/local/bin/:/bin/',
 }
 
-# Restart Nginx
+# Restart Nginx to apply the changes
 exec { 'nginx-restart':
   command => 'nginx restart',
-  path    => '/etc/init.d/'
+  path    => '/etc/init.d/',
+  require => Exec['fix--for-nginx'],
 }
