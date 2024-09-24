ab1abbae8fab4d8dcdcc51c0b304f40ffcc1b2a3
Lance <evanslance100@gmail.com>
2024-09-23 23:05:45 -0700

    refactored 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
-# This Puppet manifest installs Flask version 2.1.0 using pip3.
+# This manifest installs Flask 2.1.0 and a compatible Werkzeug version using pip3
+
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
 
+# Install a compatible version of Werkzeug
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
